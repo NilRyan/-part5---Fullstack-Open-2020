63e7bd037bf00ca4ff24b3bfc06662dd6f6f0f3e
Neil Ryan <neilryanlipaod@gmail.com>
2021-03-13 14:52:25 +0800

    5.9 *: Blog list frontend, step9
    MODIFIED blog rendering method to be sorted by number of likes first.

--- src/components/BlogsDisplay.js
+++ src/components/BlogsDisplay.js
@@ -35,7 +35,9 @@ const BlogsDisplay = ({ handleError, handleSubmittedBlog, user, blogs, handleLog
         </Toggable>
         
         {
-          blogs.map( blog =>
+          blogs
+          .sort((a, b) => (b.likes - a.likes) )
+          .map( blog =>
             <Blog key={blog.id} blog={blog}/>) 
         } 
   
